12a19bb2d0a79d89f77653f6d2618edad0588d27
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-09-16 18:06:40 +0900

    絞り込み機能の追加

--- laravel-project/app/Http/Controllers/AdminBlogController.php
+++ laravel-project/app/Http/Controllers/AdminBlogController.php
@@ -48,11 +48,40 @@ class AdminBlogController extends Controller
     }
 
     // 管理者用ブログ一覧ページ
-    public function index()
+    public function index(Request $request)
     {
-        $blogs = Blog::with('categories')->get();
-        return view('admin.blog_lists', ['blogs' => $blogs]);
+        $query = Blog::query();
+
+        // キーワード検索
+        if ($request->has('keyword') && !empty($request->input('keyword'))) {
+            $query->where('title', 'like', '%' . $request->input('keyword'). '%')
+            ->orWhere('content', 'like', '%' . $request->input('keyword'). '%');
+        }
+
+        // カテゴリー検索
+        if ($request->has('category') && !empty($request->input('category'))) {
+            $query->whereHas('categories', function($categoryQuery) use ($request) {
+                $categoryQuery->where('categories.id', $request->input('category'));
+            });
+        }
+
+        //並び順
+        if ($request->has('order')) {
+            if ($request->input('order') === 'newest') {
+                $query->orderBy('created_at', 'desc');
+            } else if ($request->input('order') === 'oldest') {
+                $query->orderBy('created_at', 'asc');
+            }
+        } else {
+            $query->orderBy('created_at', 'desc');
+        }
+
+        $blogs = $query->with('categories')->get();
+        $categories = Category::all();
+
+        return view('admin.blog_lists', ['blogs' => $blogs, 'categories' => $categories]);
     }
+
 
     // 管理者用ブログ詳細ページ
     public function show($id)
